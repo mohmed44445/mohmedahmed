d78d3197969d41c21b477989d8df14f192b3e9e5
fsgfg <fsg@ff.com>
2022-11-09 23:04:03 +0200

    sc

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM openjdk
+
+WORKDIR /app
+
+COPY ahmed.java .
+
+RUN javac ahmed.java
+
+CMD java ahmed
---
+++ mohmed.java
@@ -0,0 +1,6 @@
+public class ahmed{
+
+    public static void main(String []args){
+        System.out.println("mohmed");
+    }
+}
